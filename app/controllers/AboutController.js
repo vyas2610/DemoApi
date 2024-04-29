1ed6a8e8ffea805144342ddf60704529a61242b5
vyas2610 <deepakvyas261092@gmail.com>
2024-04-29 19:57:19 +0530

    routes are created for about & controllers are defined

--- app/controllers/AboutController.js
+++ app/controllers/AboutController.js
@@ -1,3 +1,6 @@
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+
 function index(req, res) {
     res.send({
         msg: 'About Us Page is calling..!',
@@ -8,15 +11,48 @@ function index(req, res) {
     });
 }
 
-function create(req, res) {
+async function create(req, res) {
+    let body = req.body;
+    try {
+        await prisma.category.create({
+            data: {
+                name: body.name,
+                description: body.description,
+            },
+        });
+
+        res.send({
+            msg: 'Data Added..!',
+        });
+    }
+    catch (error) {
+        res.status(500).send({
+            error,
+        });
+    }
+}
+
+function update(req, res) {
     res.send({
-        msg: 'PostMan',
-        body: req.body
+        msg: "update is calling...!"
     })
 }
 
+function destroy(req, res) {
+    res.send({
+        msg: "Delete is calling...!"
+    })
+}
 
+function show(req, res) {
+    res.send({
+        msg: "Detail is calling...!"
+    })
+}
 module.exports = {
     index,
-    create
+    create,
+    update,
+    destroy,
+    show
 }
--- utils/routes/index.js
+++ utils/routes/index.js
@@ -1,9 +1,27 @@
+/*
+    C = > CREATE
+    U = > UPDATE
+    R = > READ
+    D = > DELETE
+
+    API Functions
+    get => READ
+    post => CREATE , For add new data
+    put => UPDATE
+    delete => DELETE Record 
+*/
+
 const HomeController = require('../../app/controllers/HomeController')
 const AboutController = require('../../app/controllers/AboutController')
 function routes(app) {
     app.get('/', HomeController.index) //index for fetch data 
-    app.get('/about', AboutController.index)
-    app.get('/about', AboutController.create)
+
+    app.get('/about', AboutController.index) // for ferch data
+    app.post('/about', AboutController.create) // for add new data
+    app.put('/about', AboutController.update) // update 
+    app.delete('/about', AboutController.destroy) // delete
+    app.get('/about:id', AboutController.show) // for fetch data of a index 
+
 
 }
 
